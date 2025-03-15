19adf086b4da4eac25c7de0d2b630dd86d9a7c55
invictus841 <usedforfunny@gmail.com>
2025-03-15 10:20:44 +0100

    Added UserData DTO

---
+++ Arista/Models/UserData.swift
@@ -0,0 +1,14 @@
+//
+//  UserData.swift
+//  Arista
+//
+//  Created by Alexandre Talatinian on 14/03/2025.
+//
+
+import Foundation
+
+struct UserData: Identifiable {
+    let id: String
+    let firstName: String
+    let lastName: String
+}
