aa763dca0e3a0e9767dea338a7e203e9ad1a6fc7
invictus841 <usedforfunny@gmail.com>
2025-03-15 10:12:12 +0100

    Added SleepData DTO

---
+++ Arista/Models/SleepData.swift
@@ -0,0 +1,15 @@
+//
+//  SleepData.swift
+//  Arista
+//
+//  Created by Alexandre Talatinian on 14/03/2025.
+//
+
+import Foundation
+
+struct SleepData: Identifiable {
+    let id: String
+    let duration: Int
+    let quality: Int
+    let startDate: Date
+}
