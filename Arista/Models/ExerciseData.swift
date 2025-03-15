a1f8900505b3371698526993094b729fb21408ec
invictus841 <usedforfunny@gmail.com>
2025-03-15 10:11:51 +0100

    Added ExerciseData DTO

---
+++ Arista/Models/ExerciseData.swift
@@ -0,0 +1,16 @@
+//
+//  ExerciseData.swift
+//  Arista
+//
+//  Created by Alexandre Talatinian on 14/03/2025.
+//
+
+import Foundation
+
+struct ExerciseData: Identifiable {
+    let id: String
+    let category: String
+    let startDate: Date
+    let duration: Int
+    let intensity: Int
+}
